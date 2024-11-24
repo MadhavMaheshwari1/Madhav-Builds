85cafc03e20ef35c6bb5e3e488a88479c5ad2bd2
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-11-24 11:28:36 +0000

    Made Works Section

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           <Route path="/works" element={<Works />} />
           <Route path="/works/:projectID" element={<Project />} />
           <Route path="/about" element={<About />} />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </AnimatePresence>
     </div>
